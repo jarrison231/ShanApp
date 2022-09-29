17857f298421473bdd547e89adb6299881fc3b6f
Jarrison Miramag <jarrisonmiramagx@gmail.com>
2022-09-29 11:05:51 -0500

    Pantalla de terminos y condiciones

--- app/src/main/java/com/stasoft/shan/MainActivity.kt
+++ app/src/main/java/com/stasoft/shan/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.stasoft.shan
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stasoft.shan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonPantallaTerminos.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
     }
+
+
+
 }

1c373bba35da027f9a47ce82d73ec253181686f0
A3 PC04 <acvillota@iemsantateresita.edu.co>
2022-11-15 08:35:07 -0500

    Modificada pantalla informacion personal y agregada pantalla examen

---
+++ app/src/main/java/com/stasoft/shan/Pantalla_Examen.kt
@@ -0,0 +1,11 @@
+package com.stasoft.shan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla_Examen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_examen)
+    }
+}
